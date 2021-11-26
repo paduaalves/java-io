74d7a0b6902bea3b3038e64d91c7fc41944ad9f5
Pádua Alves <paduaalves03@gmail.com>
2021-11-26 08:55:33 -0300

    Testando serializacao

---
+++ src/br/com/alura/java/teste/io/Cliente.java
@@ -0,0 +1,53 @@
+package br.com.alura.java.teste.io;
+
+import java.io.Serializable;
+
+/**
+ * Classe que representa um cliente no Bytebank.
+ *
+ * @author Nico Steppat
+ * @version 0.1
+ */
+
+public class Cliente implements Serializable{
+	
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2465595407341116515L;
+	private String nome;
+	private String cpf;
+	private String profissao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getProfissao() {
+		return profissao;
+	}
+
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [nome=" + nome + ", cpf=" + cpf + ", profissao=" + profissao + "]";
+	}
+	
+}
